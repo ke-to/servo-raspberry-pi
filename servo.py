c2f61a5ac24f93fae03d2cc13405cd3852c02e84
ke-to <kuronokei0606@gmail.com>
2016-05-27 17:24:21 +0000

    first commit

---
+++ servo.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+pwm=GPIO.PWM(11,50)
+
+# strat
+pwm.start(0)
+time.sleep(2)
+print "servo moter start"
+
+def update(angle):
+    duty = float(angle) / 10.0 + 2.5
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(.5)
+    print "ChangeDutyCycle(%s)" % duty
+
+try:
+	while True:
+		update(90)
+		update(0)
+
+except KeyboardInterrupt:
+        pass
+
+# stop
+pwm.stop()
+GPIO.cleanup()
+
